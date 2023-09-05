7b90ba6a3c33bec4aa9167be1acb5637aca7e968
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-05 22:19:49 +0300

    Main section and header section components css done | all components implemented to the App.jsx file

--- digital-business-card/src/App.jsx
+++ digital-business-card/src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 
 export default function App() {
   return (
-    <div>
+    <div className="card-container">
       <Header />
       <Main />
       <Footer />
