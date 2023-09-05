8e4ae97721babf14f58809bb04efdc2da3a719dd
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-05 22:20:50 +0300

    Main.jsx file done

--- digital-business-card/components/Main.jsx
+++ digital-business-card/components/Main.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Main() {
-  return <h1>Main Section</h1>;
+  return (
+    <div className="main-container">
+      <h3 className="main-title">About</h3>
+      <p className="main-text">
+        First of all, I love to write code. Software development is like a
+        strategy game. Firstly, you need to analyze the resources you have.
+        After that, start developing a project based on these analyzes. I
+        believe in the power of programming to transform and improve the lives
+        of people around the world. I have been developing software using
+        different project for the last 4 years. I am mainly keen width HTML,
+        CSS, ReactJS, Javascript and Python. I have basic knowledge SQL and C#.
+      </p>
+      <h3 className="main-title">Interest</h3>
+      <p className="main-text">
+        I have a strong passion for watching films and TV series, with a
+        particular geek-level fondness for "The Lord of the Rings." I also have
+        a deep love for strategic games. I enjoy taking long walks, and during
+        these walks, I like to listen to music and podcasts.
+      </p>
+    </div>
+  );
 }
