b52d19887f6067756dab28f2b7a03ba55cc0a9f8
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-06 13:54:34 +0300

    Footer.jsx updated

--- digital-business-card/components/Footer.jsx
+++ digital-business-card/components/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Footer() {
-  return <h1>Footer Section</h1>;
+  return (
+    <div className="footer-container">
+      <img src="../public/web-logo.png" alt="logo-1" />
+
+      <img src="../public/GithubIcon.png" alt="logo-2" />
+    </div>
+  );
 }
