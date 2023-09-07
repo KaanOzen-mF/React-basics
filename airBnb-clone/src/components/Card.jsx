2f2ff7a317f6bd2dd135d8701462a79f1dfa93cf
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-07 19:56:26 +0300

    Card.jsx done

--- airBnb-clone/src/components/Card.jsx
+++ airBnb-clone/src/components/Card.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-import katieImg from "../assets/katie-img.png";
 import starImg from "../assets/starImg.png";
 
-export default function Card() {
+export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card-container">
-      <img src={katieImg} alt="" />
+      {badgeText && <div className="card-badge">{badgeText}</div>}
+      <img className="card-img" src={`src/assets/${props.coverImg}`} alt="" />
       <div className="rating-container">
         <img src={starImg} />
-        <p className="rate">5.0</p>
-        <p className="rate-info">(6) ○ USA</p>
+        <p className="rate">{props.stats.rating}</p>
+        <p className="rate-info">
+          ({props.stats.reviewCount}) ○ {props.location}
+        </p>
       </div>
-      <p className="card-info">Life lessons with Katie Zaferes</p>
+      <p className="card-info">{props.title}</p>
       <p>
-        <span className="bold-text">From $136</span> / person
+        <span className="bold-text">From ${props.price}</span> / person
       </p>
     </div>
   );
