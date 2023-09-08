329c1d40610e865c83cdc63dea8bb4c38b5e2cbe
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-08 15:01:02 +0300

    Props and all components added

--- my-travel-journey/src/App.jsx
+++ my-travel-journey/src/App.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import data from "./data";
 
 export default function App() {
+  const hero = data.map((location) => {
+    return (
+      <Hero
+        key={location.id}
+        locationName={location.title}
+        locationLink={location.googleMapsUrl}
+        locationImage={location.imageUrl}
+        locationCountry={location.location}
+        locationInfo={location.description}
+        locationStartDate={location.startDate}
+        locationEndDate={location.endDate}
+      />
+    );
+  });
+
   return (
     <div>
-      <Header />
+      <Navbar />
+      {hero}
     </div>
   );
 }
