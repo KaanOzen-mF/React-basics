20473ac7267a464bb5dbbd2a9097c13d9fe9a176
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-08 15:02:46 +0300

    data file added

---
+++ my-travel-journey/src/data.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    id: 1,
+    title: "Neuschwanstein Castle",
+    location: "Germany",
+    googleMapsUrl: "https://goo.gl/maps/nCS4CbXapsZxKEMH6",
+    startDate: "12 Jan, 2021",
+    endDate: "14 Jan, 2021",
+    description:
+      "Built atop a rock ledge over the Pöllat Gorge in the Bavarian Alps by order of Bavaria’s King Louis II (“Mad King Ludwig”). Construction began in 1868 and was never completed.",
+    imageUrl:
+      "https://www.travelandleisure.com/thmb/umcoSMJygYyG5OIYDdBPgnrJGLc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/01-neuschwanstein-castle-bavaria-NEUSCHWANSTEIN0417-273a040698f24fc1ac22e717bb3f1f0c.jpg",
+  },
+  {
+    id: 2,
+    title: "Black Forest",
+    location: "Germany",
+    googleMapsUrl: "https://goo.gl/maps/GivBwxafb3J9Ajqs5",
+    startDate: "15 Jan, 2021",
+    endDate: "18 Jan, 2021",
+    description:
+      "It is mainly a granite highland with rounded summits, although its northern part comprises forested sandstone, and it is bordered to the south by a narrow band of lower and more fertile limestone.",
+    imageUrl:
+      "https://deih43ym53wif.cloudfront.net/schwarzwald-germany-black-forest-shutterstock_1212988969.jpg_c2d9b92afe.jpg",
+  },
+  {
+    id: 3,
+    title: "Cologne",
+    location: "Germany",
+    googleMapsUrl: "https://goo.gl/maps/ycmnfvebffpcJCCK6",
+    startDate: "18 Jan, 2021",
+    endDate: "22 Jan, 2021",
+    description:
+      "German Köln, fourth largest city in Germany and largest city of the Land (state) of North Rhine–Westphalia. One of the key inland ports of Europe, it is the historic, cultural, and economic capital of the Rhineland.",
+    imageUrl:
+      "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/503000/503692-germany.jpg",
+  },
+  {
+    id: 4,
+    title: "Berchtesgaden",
+    location: "Germany",
+    googleMapsUrl: "https://goo.gl/maps/CU3TFcs4RLiEM95QA",
+    startDate: "22 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description:
+      "It is situated on the Berchtesgaden Stream in a deep valley surrounded on three sides by Austrian territory, just north of Berchtesgaden National Park.",
+    imageUrl:
+      "https://mediaim.expedia.com/destination/1/3f40a204d5834a47efa2779fd06f0b1a.jpg",
+  },
+];
