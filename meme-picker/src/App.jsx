c276cba058a60a3db8e480629858d132190a052d
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-11 10:16:24 +0300

    Header and Form componets added

--- meme-picker/src/App.jsx
+++ meme-picker/src/App.jsx
@@ -1,7 +1,11 @@
+import React from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
 function App() {
   return (
     <>
-      <p>Hello</p>
+      <Header />
+      <Form />
     </>
   );
 }
