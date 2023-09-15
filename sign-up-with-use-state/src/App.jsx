0577890ca412b2bbd078834eb947c88d4e8cfe51
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-15 11:01:31 +0300

    Sign up panel with react done

--- sign-up-with-use-state/src/App.jsx
+++ sign-up-with-use-state/src/App.jsx
@@ -1,49 +1,75 @@
-import { useState } from "react";
 import React from "react";
 
 export default function App() {
-  /**
-   * Challenge: Connect the form to local state
-   *
-   * 1. Create a state object to store the 4 values we need to save.
-   * 2. Create a single handleChange function that can
-   *    manage the state of all the inputs and set it up
-   *    correctly
-   * 3. When the user clicks "Sign up", check if the
-   *    password & confirmation match each other. If
-   *    so, log "Successfully signed up" to the console.
-   *    If not, log "passwords to not match" to the console.
-   * 4. Also when submitting the form, if the person checked
-   *    the "newsletter" checkbox, log "Thanks for signing
-   *    up for our newsletter!" to the console.
-   */
   const [form, setForm] = React.useState({
     email: "",
     password: "",
     confirmPassword: "",
-    checkbox: "",
+    checked: false,
   });
+
   function handleSubmit(event) {
     event.preventDefault();
+    if (form.password === form.passwordConfirm) {
+      console.log("Successfully signed up");
+    } else {
+      alert("Passwords do not match");
+      return;
+    }
+  }
+
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setForm((prevFormData) => ({
+      ...prevFormData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  }
+
+  function handleJoin(event) {
+    console.log(event.target.checked);
+    if (event.target.checked) {
+      alert("Thanks for joining newsletter");
+    }
   }
 
   return (
     <div className="form-container">
       <form className="form" onSubmit={handleSubmit}>
         <input
+          name="email"
           type="email"
           placeholder="Email address"
           className="form--input"
+          value={form.email}
+          onChange={handleChange}
         />
-        <input type="password" placeholder="Password" className="form--input" />
         <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          className="form--input"
+          value={form.password}
+          onChange={handleChange}
+        />
+        <input
+          name="confirmPassword"
           type="password"
           placeholder="Confirm password"
           className="form--input"
+          value={form.confirmPassword}
+          onChange={handleChange}
         />
 
         <div className="form--marketing">
-          <input id="okayToEmail" type="checkbox" />
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            name="checked"
+            onChange={handleChange}
+            checked={form.checked}
+            onClick={handleJoin}
+          />
           <label htmlFor="okayToEmail">I want to join the newsletter</label>
         </div>
         <button className="form--submit">Sign up</button>
