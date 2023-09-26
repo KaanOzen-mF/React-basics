45e2d1ad6025492c7b8817a49ddde92626a1698f
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-26 16:12:51 +0300

    Start Quiz Button work as it I want

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react";
+import Home from "./components/Home";
+import Quiz from "./components/Quiz";
+
 function App() {
-  return (
-    <>
-      <h1>Test</h1>
-    </>
-  );
+  //State to manage start to quiz game
+  const [start, setStart] = React.useState(true);
+
+  //Handle function for home page start quiz button
+  const handleStart = () => {
+    setStart((preVal) => !preVal);
+    console.log(start);
+  };
+
+  //Conditional render for JSX elements
+  return <>{start ? <Home startBtn={handleStart} /> : <Quiz />}</>;
 }
 
 export default App;
