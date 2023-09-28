143d8422b9cb9bd0eba7bdd5ac5ef0acda8c3846
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-28 16:03:08 +0300

    Render all question regarding to user input | Check answer button work after check colored answers

--- quizzical/src/components/Quiz.jsx
+++ quizzical/src/components/Quiz.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { decode } from "html-entities";
 
 export default function Quiz(props) {
+  const [selectedAnswers, setSelectedAnswers] = useState([]);
+  const [answersChecked, setAnswersChecked] = useState(false);
+  const [results, setResults] = useState(
+    Array(props.questions.length).fill(null)
+  );
+
+  const handleAnswerClick = (questionIndex, answer) => {
+    setSelectedAnswers((prevAnswers) => {
+      const newAnswers = [...prevAnswers];
+      newAnswers[questionIndex] = answer;
+      return newAnswers;
+    });
+  };
+
+  const checkAnswers = () => {
+    const newResults = selectedAnswers.map((answer, index) => {
+      const correctAnswer = props.questions[index].correct_answer;
+      return answer === correctAnswer;
+    });
+    setResults(newResults);
+    setAnswersChecked(true);
+  };
+
+  console.log("results: ", results);
+
   return (
     <div className="quiz-page">
       {props.questions.map((question, index) => {
@@ -10,16 +35,41 @@ export default function Quiz(props) {
             <p className="quiz-content">{decode(question.question)}</p>
             <ul className="quiz-answers-container">
               {props.answers[index].allAnswers.map((answer, i) => (
-                <li key={i}>{decode(answer)}</li>
+                <li
+                  key={i}
+                  onClick={() => handleAnswerClick(index, answer)}
+                  className={`${
+                    selectedAnswers[index] === answer ? "selected" : ""
+                  } ${
+                    answersChecked &&
+                    selectedAnswers[index] === answer &&
+                    results[index]
+                      ? "correct"
+                      : answersChecked &&
+                        selectedAnswers[index] === answer &&
+                        !results[index]
+                      ? "incorrect"
+                      : "other"
+                  }`}
+                >
+                  {decode(answer)}
+                </li>
               ))}
             </ul>
             <hr />
           </div>
         );
       })}
-      <button className="check-answer-btn" onClick={props.checkBtn}>
+      <button className="check-answer-btn" onClick={checkAnswers}>
         Check Answer
       </button>
+      <div>
+        {results.map((result, index) => (
+          <div key={index}>
+            Question {index + 1}: {result ? "Correct" : "Incorrect"}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
